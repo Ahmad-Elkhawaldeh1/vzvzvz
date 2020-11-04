57fc449e6c5aa236a06c7884b54b86a150d29406
Ahmad-Elkhawaldeh1 <71396577+Ahmad-Elkhawaldeh1@users.noreply.github.com>
2020-11-04 10:20:23 -0500

    Add files via upload

---
+++ ok_ok (1).ino
@@ -0,0 +1,30 @@
+
+int button0ne = 5;
+int buttonTwo = 2;
+
+void setup() {
+  pinMode(buttonOne, INPUT);
+  pinMode(buttonTwo, INPUT);
+  pinMode(12, OUTPUT); 
+  pinMode(8, OUTPUT); 
+}
+    
+void loop() {
+  int inputA = digitalRead(buttonOne);
+  int inputB = digitalRead(buttonTwo);
+
+
+  if(inputA == 1 && inputB == 0 || inputA == 0 && inputB == 1){
+      digitalWrite(12, HIGH);
+      digitalWrite(8, LOW);
+      }
+  else if(inputA == 0 && inputB == 0){
+      digitalWrite(12, LOW);
+      digitalWrite(8, LOW);
+      }
+  else if(inputA == 1 && inputB == 1){
+      digitalWrite(12, LOW);
+      digitalWrite(8, HIGH);
+      }
+  
+}
